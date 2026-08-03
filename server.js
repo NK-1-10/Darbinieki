3a5009d4fd4c525e264df65af2f44bcecb6e1449
NK-1-10 <kanepe.nikola@gmail.com>
2026-08-03 19:40:53 +0300

    Remove 'cena' from resource-types query

--- server.js
+++ server.js
@@ -339,7 +339,7 @@ app.post('/api/change-password', async (req, res) => {
 // --- 2. RESURSU PĀRVALDĪBA ---
 app.get('/api/resource-types', async (req, res) => {
     try {
-        const r = await pool.query("SELECT id, name, quantity, track_mh, vien, cena FROM resource_types ORDER BY name ASC");
+        const r = await pool.query("SELECT id, name, quantity, track_mh, vien FROM resource_types ORDER BY name ASC");
         console.log("has gotten "+  name, quantity, track_mh, vien, cena);
         res.json(r.rows); 
     } catch (err) { res.status(500).json({ error: err.message }); }
